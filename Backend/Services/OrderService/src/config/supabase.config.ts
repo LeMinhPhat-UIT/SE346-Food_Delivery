45e8e6c90fcdd01d043cb6e301ad245a2f5cedcf
Truong Quoc Thong <truongquocthong07.14@gmail.com>
2026-05-09 20:50:41 +0700

    feat: initialize and add prisma schema for order service

---
+++ Backend/Services/OrderService/src/config/supabase.config.ts
@@ -0,0 +1,13 @@
+import { createClient } from "@supabase/supabase-js";
+import { env } from "./env.config";
+
+export const supabase = createClient(
+  env.SUPABASE_PROJECT_URL,
+  env.SUPABASE_SERVICE_ROLE_KEY ?? env.SUPABASE_ANON_KEY,
+  {
+    auth: {
+      persistSession: false,
+      autoRefreshToken: false,
+    },
+  }
+);
---
+++ Backend/Services/OrderService/src/utils/response.ts
@@ -0,0 +1,30 @@
+import { Response } from "express";
+import { HTTP_STATUS } from "../constants/httpStatus";
+
+export default class Send {
+  static success(
+    res: Response,
+    data: unknown,
+    message = "success",
+    statusCode: number = HTTP_STATUS.OK
+  ) {
+    return res.status(statusCode).json({
+      ok: true,
+      message,
+      data,
+    });
+  }
+
+  static error(
+    res: Response,
+    data: unknown,
+    message = "error",
+    statusCode: number = HTTP_STATUS.INTERNAL_SERVER_ERROR
+  ) {
+    return res.status(statusCode).json({
+      ok: false,
+      message,
+      data,
+    });
+  }
+}
